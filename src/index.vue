d5e841a00a02cd9ed43f44e57f2e1f6a83b07bbe
Peng Zhong <peng@nylira.com>
2017-01-19 13:13:46 +0900

    init

---
+++ src/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pz-article-body">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pz-article-body',
+  props: ['content']
+}
+</script>
+
+<style>
+
+<style src="./style.css"></style>
